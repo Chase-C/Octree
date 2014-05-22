b907465c2819f2352f7230dbc286547be428acbd
Chase Cummings <chaseecummings@gmail.com>
2014-05-22 00:30:47 -0700

    Cleaned up some stuff

--- src/Main.hs
+++ src/Main.hs
@@ -7,28 +7,30 @@ import Vec3D
 
 --------------------------------------------------------------------------------
 
+xs :: [(Int, Vec3D)]
+xs = (1,  Vec3D (  1,    2,    3))
+   : (2,  Vec3D ((-1), (-2), (-3)))
+   : (3,  Vec3D ((-9),   2,    3))
+   : (4,  Vec3D (  5,  (-2),   5))
+   : (5,  Vec3D (  2,    6,    6))
+   : (18, Vec3D (  1,    2,  (-3)))
+   : (6,  Vec3D ((-5), (-5),   4))
+   : (7,  Vec3D (  5,  (-4), (-5)))
+   : (8,  Vec3D ((-6),   5,  (-5)))
+   : (9,  Vec3D (  5,    6,    5))
+   : (0,  Vec3D ((-5), (-5), (-6)))
+   : (19, Vec3D (2, 2, 2))
+   : (11, Vec3D (2, 2, 6))
+   : (12, Vec3D (6, 6, 6))
+   : (13, Vec3D (6, 2, 2))
+   : (14, Vec3D (2, 6, 2))
+   : []
+
+tree :: O.Octree Int
+tree = O.splitWith (O.insertList (O.emptyOctree (Vec3D (0, 0, 0)) 16 :: O.Octree Int) xs) ((> 8) . O.count)
+
 main :: IO ()
 main = do
-    let a = O.emptyOctree (Vec3D (0, 0, 0)) 16 :: O.Octree Int
-        xs = (1,  Vec3D (  1,    2,    3))
-           : (2,  Vec3D ((-1), (-2), (-3)))
-           : (3,  Vec3D ((-9),   2,    3))
-           : (4,  Vec3D (  5,  (-2),   5))
-           : (5,  Vec3D (  2,    6,    6))
-           : (18, Vec3D (  1,    2,  (-3)))
-           : (6,  Vec3D ((-5), (-5),   4))
-           : (7,  Vec3D (  5,  (-4), (-5)))
-           : (8,  Vec3D ((-6),   5,  (-5)))
-           : (9,  Vec3D (  5,    6,    5))
-           : (0,  Vec3D ((-5), (-5), (-6)))
-           : (19, Vec3D (2, 2, 2))
-           : (11, Vec3D (2, 2, 6))
-           : (12, Vec3D (6, 6, 6))
-           : (13, Vec3D (6, 2, 2))
-           : (14, Vec3D (2, 6, 2))
-           : [] :: [(Int, Vec3D)]
-        tree = O.splitWith (O.insertList a xs) ((> 8) . O.count)
     putStrLn $ O.prettyPrint tree
-
     putStrLn $ show $ O.kNearestNeighbors tree (Vec3D (8, 8, 8)) 5 10
     putStrLn $ show $ O.getRadiusObjects  tree (Vec3D (8, 8, 8))   6
